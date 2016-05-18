b7eacba4da7e80a4f307ebd636ebf6db060cb0c6
rregeA <rigert.alekaj@outlook.com>
2016-05-19 00:03:25 +0200

    final

--- rec-find.pl
+++ rec-find.pl
@@ -5,20 +5,12 @@ use warnings;
 use File::Copy;
 use 5.010;
 use Getopt::Std;
-use Getopt::Long;
+use Net::OpenSSH;
 
 
-	#our $numri = "23";
-	#our $server = "5";
-	#my $help = "";
-	#GetOptions( 'rec|r' => \$numri,
-	#			'server|s' => \$server,
-	#	);
-	#print $numri;
-	#print $server;
 	
 	my %options =();
-	getopts("hr:s:", \%options);
+	getopts("h:r:s:", \%options);
 
 	if($options{h})
 	{
@@ -32,11 +24,18 @@ use Getopt::Long;
 		exit;
 	}
 
+	if($options{r} && $options{s})
+	{
+		find_rec_server();
+		exit;
+	}
 
-	#kontrollo nqs eshte dhen numri
-	#if ($numri eq "" or $#ARGV == -1){
-	#	die("Error : duhet dhene rec qe do kerkohet,perdor rec-find -h per me shume.\n")
-	#}
+	if(!$options{r})
+	{
+		print "Error : Duhet dhene numri i rregistrimit qe do kerkohet, shiko rec-find -h per me shume.\n";
+	}
+
+
 
 	sub show_help 
 	{
@@ -52,18 +51,9 @@ use Getopt::Long;
 
 	sub find_rec_local
 	{
-		my $numri =;
-		GetOptions('r' => \$numri) or die "nuk eci vlla";
-		#print $numri;
-		#my $num_args = $#ARGV +1;
-		#if ($num_args != 1){
-		#	print "Error : Numer jo i sakte i argumenteve, perdor rec_find -h\n";
-		#	exit;
-		#}
-		#print $numri;
-		#exit;
-		my $read = $numri;
-		my $comand = "locate $read";
+		
+		my $numri = $options{r};
+		my $comand = "locate $numri";
 		my @location = `$comand`;
 
 		chomp @location;
@@ -79,6 +69,40 @@ use Getopt::Long;
 			copy("$line", '/public/it-software/Rigert/rec')
 		}
 	}
+
+	sub find_rec_server
+	{
+				print "kodi per serverat sipe.\n";
+				my $host = $options{s};
+				my $host_plote = "root@"."$host";
+				my @keyfile = ("/root/.ssh/id_rsa");
+				
+				my $numri = $options{r};
+		        
+		        my $cmd = "locate $numri";
+
+				
+				my $ssh = Net::OpenSSH->new($host_plote);
+
+
+				
+				my @location = $ssh->capture("locate $numri");
+				chomp @location;
+
+				my $num_lines = $#location +1;
+					if ($num_lines == 0){
+						print "Nuk u gjet asnje rregistrim.\n";
+						exit;
+					}
+				foreach my $line (@location)
+					{
+						
+						$ssh->system("scp $line arch:/public/it-software/Rigert/rec");
+					}
+				
+	}
+
+
 
 
 
